a866c81361ecd37f06ab232bda69d2618de1f504
shahrukhnasir <shahrukhnasir>
2024-01-18 12:54:40 +0500

    update vercel

--- src/pages/shop/product/[pid].js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const DynamicProductDetails = dynamic(() => import("../../../components/ProductDetail/ProductDetail"));
-
-const ProductDetails = () => {
-  return <DynamicProductDetails/>;
-};
-
-export default ProductDetails;
