2cdfe482d952baace31333f12ffab4db71fdaa7c
shahrukhnasir <shahrukhnasir0108@gmail.com>
2024-02-08 16:57:58 +0500

    check middleWare

--- src/pages/appointment-waiting-list.js
+++ src/pages/appointment-waiting-list.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import WaitingList from '../AllPages/Profile/WaitingList/WaitingList'
-import withAuth from '../../WithAuth'
+import withAuth from '../components/withAuth'
 const AppointmentWaitinglist =()=> {
   return (
     <>
